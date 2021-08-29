d37ed6d199b2f129c4cf565d9b64606f327ebdbc
Khrystyna Pryslupska <christina_seniuta@ukr.net>
2021-08-30 00:02:36 +0300

    update pipe

--- src/app/pipe/search-filter.pipe.ts
+++ src/app/pipe/search-filter.pipe.ts
@@ -4,11 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'searchFilter',
 })
 export class SearchFilterPipe implements PipeTransform {
-  transform(users: any, value: string): any {
-    if (!users) return null;
-
-    if (!value) return users;
-
-    return users.filter((data: any) => data.name.includes(value));
+  transform(data: any[], value: string, prop: any): any {
+    if (!data) return null;
+    if (!value) return data;
+    return data.filter((elem: any) => elem[prop].includes(value));
   }
 }
